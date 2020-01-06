a493de0cefdd8001b72565e644b3b512aec4c533
tom3108 <t.madziara@gmail.com>
2020-01-06 20:58:17 +0100

    Link detail view

--- rm/urls.py
+++ rm/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('',views.art_list, name='art_list')
+	path('',views.art_list, name='art_list'),
+	path('art/<int:pk>/', views.art_detail, name='art_detail'),
 	]
--- rm/views.py
+++ rm/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Art
 
@@ -6,3 +6,7 @@ from .models import Art
 def art_list (request):
 	arts = Art.objects.order_by('created_date')
 	return render(request, 'rm/art_list.html', {'arts':arts})
+
+def art_detail(request, pk):
+    art = get_object_or_404(Art, pk=pk)
+    return render(request, 'rm/art_detail.html', {'art': art})
