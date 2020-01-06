a539ba3a5dde75ff63439de2eb920b41893635a9
tom3108 <t.madziara@gmail.com>
2020-01-06 14:12:15 +0100

    query sets. dynamic variable

--- rm/views.py
+++ rm/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Art
 
 # Create your views here.
-def articles (request):
-	return render(request, 'rm/articles.html', {})
+def art_list (request):
+	arts = Art.objects.order_by('created_date')
+	return render(request, 'rm/art_list.html', {'arts':arts})
